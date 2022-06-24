01049e0695ecbd160bac7349e23ada4d275a63c5
Ariel <arielpenna@gmail.com>
2022-06-23 23:49:02 -0300

    app gastos,back funcionando

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const userRoutes = require('./routes/users')
+const balanceRoutes = require('./routes/balance')
+const cors = require('cors')
+const connectDB = require('./config/db')
+
+require('dotenv').config()
+connectDB()
+
+app.use(cors())
+app.use(express.json())
+
+//3. Rutas
+app.use('/balance', balanceRoutes)
+app.use('/usuario', userRoutes)
+app.get('/', (req, res) => res.send('UCAMP API'))
+
+// 4. SERVIDOR
+app.listen(process.env.PORT, () => {
+	console.log('El servidor está corriendo en 4000')
+})
